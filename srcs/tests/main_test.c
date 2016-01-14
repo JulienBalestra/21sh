02b1095e30ab3f75f0a67bd37833354ab20507ec
Julien Balestra <julien.balestra@gmail.com>
2016-01-14 11:26:24 +0100

    Debug opt

--- srcs/tests/main_test.c
+++ srcs/tests/main_test.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include "../../libft/includes/libft.h"
 #include "../../includes/ast.h"
 #include "../../includes/test.h"
@@ -7,37 +8,36 @@ int ast_testing(char *str, int debug)
 {
 	t_ast *ast;
 
-	ft_putstr("\n\n [START] \"");
+	ft_putstr("\n\n[START] \"");
 	ft_putstr(str);
-	ft_putstr("\" [START] \n\n");
+	ft_putstr("\" [START]\n\n");
 	if (debug)
-		ft_putstr(" [ START ] ast_build [ START ]\n");
+		ft_putstr("[ START ] ast_build [ START ]\n");
 	ast = ast_build(ft_strdup(str));
 	if (debug)
 	{
-		ft_putstr(" [ ENDED ] ast_build [ ENDED ]\n");
-		ft_putstr("rrrrrrr [ START ] ast_read [ START ] rrrrrrr\n");
+		ft_putstr("[ ENDED ] ast_build [ ENDED ]\n\n");
+		ft_putstr("rrrrrrrr\n");
 		ast_read(ast);
 	}
 	if (debug)
-		ft_putstr("\nrrrrrrr [ END ] ast_read [ END ] rrrrrrr\n");
-	ft_putstr("xxxxxxxx [ START ] ast_exec [ START ] xxxxxxxx\n");
+		ft_putstr("\nrrrrrrrr\n\n");
+	ft_putstr("xxxxxxxx\n");
 	ast_exec(ast);
-	ft_putstr("xxxxxxxx [ ENDED ] ast_exec [ ENDED ] xxxxxxxx\n\n");
+	ft_putstr("xxxxxxxx\n\n");
 	if (debug)
-		ft_putstr(" [ START ] ast_clean [ START ]\n");
+		ft_putstr("[ START ] ast_clean [ START ]\n");
 	ast_clean(ast);
 	if (debug)
-		ft_putstr(" [ ENDED ] ast_clean [ ENDED ]\n");
-	ft_putstr(" [ENDED] \"");
+		ft_putstr("[ ENDED ] ast_clean [ ENDED ]\n\n");
+	ft_putstr("[ENDED] \"");
 	ft_putstr(str);
 	ft_putstr("\" [ENDED]\n\n");
 	return (0);
 }
 
-int ast_testing_main(void)
+int ast_testing_main(int debug)
 {
-	int debug = 0;
 	ft_putstr("----- [ START ] AST_TESTING [ START ] -----");
 
 	ast_testing("/bin/echo simple", debug);
@@ -53,22 +53,25 @@ int ast_testing_main(void)
 	return (0);
 }
 
-int all(void)
+int all(int debug)
 {
 	int ret;
 
 	ret = 0;
-	ret = ret + ast_testing_main();
+	ret = ret + ast_testing_main(debug);
 	return (ret);
 }
 
 int main(int ac, char **av)
 {
-	int ret;
+	int ret = 0;
+	int debug = 0;
 
-	ret = 0;
+	if (getenv("21_DEBUG"))
+		debug = 1;
+
 	if (ac == 1)
-		all();
+		all(debug);
 	else if (ac > 2)
 	{
 		ft_putstr_fd("too much args\n", 2);
@@ -76,10 +79,10 @@ int main(int ac, char **av)
 	}
 	else if (ft_strcmp(av[1], "all") == 0)
 	{
-		ret = all();
+		ret = all(debug);
 	}
 	else if (ft_strcmp(av[1], "ast") == 0)
-		ret = ast_testing_main();
+		ret = ast_testing_main(debug);
 	else
 	{
 		ft_putstr_fd("unknown args [", 2);
