f71bdf03080f7cf27938a5cbd3b4baf65ddfbc48
JulienBalestra <julien.balestr@gmail.com>
2016-03-31 14:24:12 +0200

    Norme

--- srcs/find_bin.c
+++ srcs/find_bin.c
@@ -1,97 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   find_bin.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jubalest <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/31 14:17:31 by jubalest          #+#    #+#             */
+/*   Updated: 2016/03/31 14:17:32 by jubalest         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "../includes/minishell.h"
 #include "../libft/includes/libft.h"
 
-char *ft_strrev(char *str)
+char	*ft_strrev(char *str)
 {
-    int i;
-    size_t len;
-    char *rev;
+	int		i;
+	size_t	len;
+	char	*rev;
 
-    i = 0;
-    rev = NULL;
-    if (str && ((len = ft_strlen(str))))
-    {
-        if ((rev = (char *)malloc(sizeof(char) * len)))
-        {
-            len--;
-            while (len)
-            {
-                rev[i] = str[len];
-                i++;
-                len--;
-            }
-            rev[i] = '\0';
-        }
-    }
-    return (rev);
+	i = 0;
+	rev = NULL;
+	if (str && ((len = ft_strlen(str))))
+	{
+		if ((rev = (char *)malloc(sizeof(char) * len)))
+		{
+			len--;
+			while (len)
+			{
+				rev[i] = str[len];
+				i++;
+				len--;
+			}
+			rev[i] = '\0';
+		}
+	}
+	return (rev);
 }
 
-char *get_prev_chars(t_term *term)
+char	*get_prev_chars(t_term *term)
 {
-    char *buf;
-    char *rev;
-    int i;
+	char	*buf;
+	char	*rev;
+	int		i;
 
-    i = 0;
-    if ((buf = ft_strnew(COMP_SIZE)))
-    {
-        term = get_current_cursor(term);
-        if (term->c == ' ' && term->prev)
-            term = term->prev;
-        while (term && i < COMP_SIZE && term->c != ' ')
-        {
-            buf[i] = (char)term->c;
-            term = term->prev;
-            i++;
-        }
-    }
-    rev = ft_strrev(buf);
-    free(buf);
-    return (rev);
+	i = 0;
+	if ((buf = ft_strnew(COMP_SIZE)))
+	{
+		term = get_current_cursor(term);
+		if (term->c == ' ' && term->prev)
+			term = term->prev;
+		while (term && i < COMP_SIZE && term->c != ' ')
+		{
+			buf[i] = (char)term->c;
+			term = term->prev;
+			i++;
+		}
+	}
+	rev = ft_strrev(buf);
+	free(buf);
+	return (rev);
 }
 
-void    play_next(t_sh *shell, t_term *term, char *str, char *map_name)
+void	play_next(t_sh *shell, t_term *term, char *str, char *map_name)
 {
-    size_t  len;
-    char *end;
+	size_t	len;
+	char	*end;
 
-    /*//DEBUG
-        ft_putstr_fd("m:", DEBUG_FD);
-        ft_putendl_fd(map_name, DEBUG_FD);\
-    //DEBUG*/
-
-    len = ft_strlen(str);
-    end = &map_name[len];
-    while (end && *end)
-    {
-        tc_continue_process_key(shell, term, (long)*end);
-        end++;
-    }
-    tc_continue_process_key(shell, term, (long)' ');
+	len = ft_strlen(str);
+	end = &map_name[len];
+	while (end && *end)
+	{
+		tc_continue_process_key(shell, term, (long)*end);
+		end++;
+	}
+	tc_continue_process_key(shell, term, (long)' ');
 }
 
-void    find_next(t_sh *shell, t_term *term, char *str)
+void	find_next(t_sh *shell, t_term *term, char *str)
 {
-    t_bin   *map;
+	t_bin	*map;
 
-    map = shell->map;
-    while (map && map->next)
-    {
-        if (starts_with(str, map->name))
-        {
-            if (map->prev && starts_with(str, map->prev->name))
-            {
-              ;
-            }
-            else if (map->next && starts_with(str, map->next->name))
-                ;
-            else
-            {
-                play_next(shell, term, str, map->name);
-                return ;
-            }
-        }
-        map = map->next;
-    }
+	map = shell->map;
+	while (map && map->next)
+	{
+		if (starts_with(str, map->name))
+		{
+			if (map->prev && starts_with(str, map->prev->name))
+			{
+				;
+			}
+			else if (map->next && starts_with(str, map->next->name))
+				;
+			else
+			{
+				play_next(shell, term, str, map->name);
+				return ;
+			}
+		}
+		map = map->next;
+	}
 }
